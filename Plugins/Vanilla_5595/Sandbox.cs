931cc93def46dd7d846a3d3afaacbac72f1ea2fe
Barncastle <spearman.tom@gmail.com>
2019-03-20 21:10:03 +0000

    added 1.12.x

---
+++ Plugins/Vanilla_5595/Sandbox.cs
@@ -0,0 +1,27 @@
+﻿using Common.Interfaces;
+using Common.Interfaces.Handlers;
+using Vanilla_5595.Handlers;
+
+namespace Vanilla_5595
+{
+    public class Sandbox : ISandbox
+    {
+        public static Sandbox Instance { get; } = new Sandbox();
+
+        public string RealmName { get; set; } = "|cFF00FFFFVanilla (1.12.X) Sandbox";
+        public int Build { get; set; } = 5595;
+        public int RealmPort { get; set; } = 3724;
+        public int RedirectPort { get; set; } = 9002;
+        public int WorldPort { get; set; } = 8129;
+
+        public IOpcodes Opcodes { get; set; } = new Opcodes();
+
+        public IAuthHandler AuthHandler { get; set; } = new AuthHandler();
+        public ICharHandler CharHandler { get; set; } = new CharHandler();
+        public IWorldHandler WorldHandler { get; set; } = new WorldHandler();
+
+        public IPacketReader ReadPacket(byte[] data, bool parse = true) => new PacketReader(data, parse);
+
+        public IPacketWriter WritePacket() => new PacketWriter();
+    }
+}
