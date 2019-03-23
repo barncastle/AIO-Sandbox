cb29e40cdade997cdc4a5c30a3d86b062c886fec
Barncastle <spearman.tom@gmail.com>
2019-03-23 14:23:50 +0000

    added 2.0.0.5965

---
+++ Plugins/TBC_5965/PacketReader.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Common.Cryptography;
+using Common.Network;
+
+namespace TBC_5965
+{
+    public class PacketReader : BasePacketReader
+    {
+        private const int SHA_DIGEST_LENGTH = 40;
+
+        public PacketReader(byte[] data, bool parse = true) : base(data)
+        {
+            if (parse)
+            {
+                Decode(ref data);
+                ushort size = BitConverter.ToUInt16(data, 0);
+                Size = (ushort)((size >> 8) + ((size & 0xFF) << 8) + 2);
+                Opcode = BitConverter.ToUInt32(data, 2);
+
+                Position = 6;
+            }
+        }
+
+        private void Decode(ref byte[] data)
+        {
+            if (!ClientAuth.Encode || data.Length < 6)
+                return;
+
+            for (int i = 0; i < 6; i++)
+            {
+                ClientAuth.Key[1] %= SHA_DIGEST_LENGTH;
+                byte x = (byte)((data[i] - ClientAuth.Key[0]) ^ ClientAuth.SS_Hash[ClientAuth.Key[1]]);
+                ++ClientAuth.Key[1];
+                ClientAuth.Key[0] = data[i];
+                data[i] = x;
+            }
+        }
+    }
+}
---
+++ Plugins/TBC_5965/Sandbox.cs
@@ -0,0 +1,29 @@
+﻿using Common.Constants;
+using Common.Interfaces;
+using Common.Interfaces.Handlers;
+using TBC_5965.Handlers;
+
+namespace TBC_5965
+{
+    public class Sandbox : ISandbox
+    {
+        public static Sandbox Instance { get; } = new Sandbox();
+
+        public string RealmName => "TBC Alpha (2.0.0.5965) Sandbox";
+        public Expansions Expansion => Expansions.TBC;
+        public int Build => 5965;
+        public int RealmPort => 3724;
+        public int RedirectPort => 9002;
+        public int WorldPort => 8129;
+
+        public IOpcodes Opcodes => new Opcodes();
+
+        public IAuthHandler AuthHandler => new AuthHandler();
+        public ICharHandler CharHandler => new CharHandler();
+        public IWorldHandler WorldHandler => new WorldHandler();
+
+        public IPacketReader ReadPacket(byte[] data, bool parse = true) => new PacketReader(data, parse);
+
+        public IPacketWriter WritePacket() => new PacketWriter();
+    }
+}
