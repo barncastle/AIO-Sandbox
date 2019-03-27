05d9be633adaddd95555cb66777ff22fd6cc9a35
Barncastle <spearman.tom@gmail.com>
2019-03-27 13:38:20 +0000

    added 2.1.0.6577

---
+++ Plugins/TBC_6577/Sandbox.cs
@@ -0,0 +1,29 @@
+﻿using Common.Constants;
+using Common.Interfaces;
+using Common.Interfaces.Handlers;
+using TBC_6577.Handlers;
+
+namespace TBC_6577
+{
+    public class Sandbox : ISandbox
+    {
+        public static Sandbox Instance { get; } = new Sandbox();
+
+        public string RealmName => "TBC Beta (2.1.0.6577) Sandbox";
+        public Expansions Expansion => Expansions.TBC;
+        public int Build => 6577;
+        public int RealmPort => 3724;
+        public int RedirectPort => 9002;
+        public int WorldPort => 8129;
+
+        public IOpcodes Opcodes => new Opcodes();
+
+        public IAuthHandler AuthHandler => new AuthHandler();
+        public ICharHandler CharHandler => new CharHandler();
+        public IWorldHandler WorldHandler => new WorldHandler();
+
+        public IPacketReader ReadPacket(byte[] data, bool parse = true) => new PacketReader(data, parse);
+
+        public IPacketWriter WritePacket() => new PacketWriter();
+    }
+}
